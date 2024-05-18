17e59992c298a8b67ec93e3f39643ee76fd0708a
MatiwosD <matiwos.desalegn@hst-et.com>
2024-05-18 15:34:47 +0300

    Working on the modeling handling missing value

--- src/data_clean_processing.py
+++ src/data_clean_processing.py
@@ -7,18 +7,18 @@ class DataCleanProcessing:
     def clean_missing_values(self):
         # Drop columns with more than 50% missing values
         threshold = len(self.data) * 0.5
-        data_cleaned = self.data.dropna(thresh=threshold, axis=1)
+        data_cleaned = self.data.dropna(thresh=threshold, axis=1).copy()  # Create a copy here to avoid SettingWithCopyWarning
         
         # Fill remaining missing values with the median for numerical columns or mode for categorical columns
         for column in data_cleaned.columns:
             if data_cleaned[column].dtype == 'object':
                 # Fill missing values with mode for categorical columns
                 mode = data_cleaned[column].mode()[0]
-                data_cleaned[column] = data_cleaned[column].fillna(mode)
+                data_cleaned.loc[:, column] = data_cleaned.loc[:, column].fillna(mode)
             else:
                 # Fill missing values with median for numerical columns
                 median = data_cleaned[column].median()
-                data_cleaned[column] = data_cleaned[column].fillna(median)
+                data_cleaned.loc[:, column] = data_cleaned.loc[:, column].fillna(median)
         
         self.data = data_cleaned
         return data_cleaned
